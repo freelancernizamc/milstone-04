dfee6bbf7363d906fc990dfb27e70adcfa285173
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-01-26 23:20:36 +0600

    module 17 added

---
+++ js/mod17.js
@@ -0,0 +1,81 @@
+var onionPrice = 30;
+var eggPrice = 20;
+// console.log(onionPrice);
+
+
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+var totalPrice = onionPrice/eggPrice;
+console.log(totalPrice);
+// variable Declaration
+var tomatoPrice = 25;
+// variable update
+tomatoPrice = 30;
+var price1 = 31;
+var price2 = 20;
+// set a new value
+// price1= 35;
+// add some value to the existing value
+price1 = price1 + 10;
+console.log(price1);
+var price1 = price1*20;
+price1 +=20;
+// console.log(price1);
+// addition to the same variable shorthand
+// += means adding some value to the existing value
+var firstName = 'Jannie';
+var lastName = 'Kim';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+var price1 = '51';
+var price2 = '20';
+console.log(price1 + price2);
+var price1 = parseInt(price1);
+console.log(price1);
+var price5 = 39.61;
+var price5 = parseFloat(price5);
+// console.log(price5);
+var price = 200;
+console.log(typeof price);
+var price ='10';
+console.log(typeof price);
+var isHappy = true;
+// console.log(typeof isHappy);
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(2);
+console.log(sum);
+var mangos = 11;
+var person = 2;
+// console.log(mangos % person);
+
+// Practice Problem 1
+var momGave = 1000;
+var cost = 700;
+var shopkeperReturn = momGave-cost;
+console.log(shopkeperReturn);
+// Practice Problem 2
+var mathMarks = 75.25;
+var bioMarks = 65;
+var chemMarks = 80;
+var phyMarks = 35.45;
+var banMarks = 99.50;
+var totalMarks = mathMarks +bioMarks + chemMarks + phyMarks + banMarks;
+var totalSubject = 5;
+var avarageMarks= totalMarks/totalSubject;
+
+
+avarageMarks = avarageMarks.toFixed(2);
+console.log(avarageMarks);
+// Practice 03
+var one = "I am going to be";
+var two = "an awesome web developer";
+var oneLine = one + two;
+console.log(oneLine);
+
+// Practice 04
+var motherGave = 119;
+var divide = 5;
+var remainder = motherGave % divide;
+console.log(remainder);
