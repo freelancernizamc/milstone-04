310cef8328f647bcfb545a7682b08ee7f4e970d0
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-01-27 11:55:11 +0600

    mod18.js added

--- js/mod17.js
+++ js/mod17.js
@@ -5,21 +5,26 @@ var eggPrice = 20;
 
 var totalPrice = onionPrice + eggPrice;
 // console.log(totalPrice);
+
 var totalPrice = onionPrice/eggPrice;
 console.log(totalPrice);
+
 // variable Declaration
 var tomatoPrice = 25;
 // variable update
+
 tomatoPrice = 30;
 var price1 = 31;
 var price2 = 20;
 // set a new value
+
 // price1= 35;
 // add some value to the existing value
 price1 = price1 + 10;
 console.log(price1);
 var price1 = price1*20;
 price1 +=20;
+
 // console.log(price1);
 // addition to the same variable shorthand
 // += means adding some value to the existing value
@@ -35,12 +40,14 @@ console.log(price1);
 var price5 = 39.61;
 var price5 = parseFloat(price5);
 // console.log(price5);
+
 var price = 200;
 console.log(typeof price);
 var price ='10';
 console.log(typeof price);
 var isHappy = true;
 // console.log(typeof isHappy);
+
 var num1 = 0.1;
 var num2 = 0.2;
 var sum = num1 + num2;
---
+++ js/mod18.js
@@ -0,0 +1,36 @@
+// number type variable
+var sunglassPrice = 120;
+// string type variable
+var specialOneName = "Ariana Grande";
+// boolean type variable
+var isSingle = true;
+// convert: parseInt, parseFloat
+// Video-2:
+// array declaration
+var friendsAge = [11, 21, 45, 17, 14];
+var picnicFee = [5000, 2000, 4000];
+var nayikas =['mahi', "opu", 'shabnur'];
+console.log(nayikas);
+
+// Video 03
+var numbers = [54, 68, 78, 89, 98];
+// index= 1, 2 3 4
+// get elements by index
+// console.log(nayikas[0]);
+// array element set
+numbers[1] = 77;
+// console.log(numbers);
+// 3. find index of an element
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);
+
+// Video 04
+var numbers = [78, 45, 98, 99];
+// use push to add element
+numbers.push(66);
+console.log(numbers);
+// use pop to remove last element
+numbers.pop();
+console.log(numbers);
+var element = numbers.pop();
+console.log(element);
