14df0654ecc52db3b0a0cb69fe5cdcb576f9a557
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-02-07 01:07:33 +0600

    assignment4.js file added

---
+++ js/assignment_4.js
@@ -0,0 +1,88 @@
+// Assignment 04
+
+// Problem 01
+// Solutions
+
+function mindGame(number){
+    result = (number * 3 + 10)/2 - 5;
+    return result;
+
+}
+
+const game = mindGame(50);
+console.log(game);
+
+// Descriptions: The function mindGame takes a positive number num as input and returns the result of the calculation described in this problem. The calculation is performed using a single expression and the function returns the result.
+
+
+
+// Problem 02
+// Solution:
+
+
+function evenOdd(str) {
+    return str.length % 2 === 0 ? 'even' : 'odd';
+  }
+  const getEvenOrOdd = evenOdd('chatgpt');
+  console.log(getEvenOrOdd); 
+
+//   Description:
+//   The function evenOdd takes a string str as input and returns 'even' if the number of characters in the string is even, and 'Odd' if it's odd. The length of the string is calculated using the .length property, and the modulo operator % is used to determine if it's even or odd. The ternary operator ? is used to return 'even' if the length is even and 'odd' otherwise.
+
+
+
+//   Problem 03
+// Solution
+
+
+function isLGSeven(number){
+    let i = number - 7;
+    if(i < 7){
+        return i;
+    }
+    else{
+        return number*2;
+    }
+}
+const getValue = isLGSeven(15);
+console.log(getValue);
+
+
+// Descriptions: The function isLGSeven takes a number as input and returns the result of the calculation described in the problem. The variable i is used to store the absolute value of the difference between number and 7. The ternary operator ? is used to return i if it is less than 7, and return number * 2 otherwise. 
+
+
+// Problem 04
+// Solution:
+
+
+function findingBadData(arr) {
+    let noOfBadData = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < 0) {
+        noOfBadData++;
+      }
+    }
+    return noOfBadData;
+  }
+ const getData = findingBadData([ -4, -9, -5, -33, -55 ]);
+ console.log(getData);
+
+// Descriptions: The function findingBadData takes an array arr as input and returns the number of negative elements in the array. A variable count is used to keep track of the number of negative elements. The for loop iterates over the elements of the array, and the if statement checks if each element is negative. If an element is negative, the count variable is incremented by 1. Finally, the function returns the value of noOfBadData.
+
+//  Problem 05
+// Solution:
+
+
+function gemsToDiamond(f1gem, f2gem, f3gem){
+    let  i= f1gem*21 + f2gem*32 +f3gem*43;
+      if(i<=1000*2){
+          return i;
+      }
+      else if(i >1000*2){
+          return (i-2000);
+      }
+  }
+  const getDiamond = gemsToDiamond(100, 5, 1);
+  console.log(getDiamond);
+
+//   Descriptions: The function gemsToDiamond takes three numbers f1gem, f2gem, and f3gem as input and returns the total number of diamonds you will get by combining the gems of all your friends. The variable total is used to store the i number of diamonds, which is calculated by multiplying the number of gems for each friend by the power of gems per friend and summing the results. The ternary operator ? is used to return i - 2000 if total is greater than 1000*2, and return total otherwise.
--- js/milstone4.js
+++ js/milstone4.js
@@ -158,3 +158,211 @@ console.log(sqareFour);
     console.log(name);
 })('Nizam');
 
+// for loop
+
+// for(let i = 0; i <=10; i++){
+// for(let i = 0; i <10; i=i+1){
+// for(let i = 0; i <10; i+=2){
+for(let i = 9; i >0; i--){
+    if(i===5) break;//continue;
+    console.log(i);
+}
+
+let myArray = ['one', 'two', 'three', 'four', 'five'];
+// for(let i = 0; i< myArray.length; i++){
+//  for(let i= myArray.length-1; i >=0; i--){
+//     console.log(myArray[i]);
+// }
+// while loop
+
+// let i= 0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+
+let i=0;
+while(i< myArray.length){
+    console.log(myArray[i]);
+    i++;
+}
+
+// Assignment
+// Problem 01: তোমাকে একটা function দেওয়া হবে called mindGame(” যা ইনপুট হিসেবে একটা positive number নিবে। 
+// এখন তোমার task : তোমাকে সেই নাম্বার এর সাথে 3 গুন করে, তারপর 10 যোগ করে, তারপর 2 দিয়ে ভাগ করে, তারপর 5 বিয়োগ করে, যা আউটপুট আসবে তা return করতে হবে। Sample Input & Output
+// Input: 5
+
+// Output: 7.5
+
+
+
+// Input: 50
+
+// Output: 65
+
+
+
+// Input: -33
+
+// Output: -25.5
+
+function mindGame(number){
+    result = (number * 3 + 10)/2-5;
+    return result;
+
+}
+const game = mindGame(50);
+console.log(game);
+
+
+// Problem 02: তোমাকে একটা function দেওয়া হবে called evenOdd()”. এটা ইনপুট হিসেবে একটা string নিবে। 
+
+function evenOdd(str) {
+    return str.length % 2 === 0 ? 'Even' : 'Odd';
+  }
+  const getEvenOrOdd = evenOdd('chatgpt');
+  console.log(getEvenOrOdd); 
+  
+
+
+// এখন তোমার task: String এ total কয়টা character আছে তার উপর ভিত্তি করে আউটপুট দিতে হবে। আউটপুট হবে ‘Even’ অথবা ‘Odd’ ।Sample Input & Output:-
+
+
+
+// Input: ‘Phero’
+
+// Output: Odd
+
+
+
+// Input: ‘Batch7’
+
+// Output: Even
+
+
+
+// Input: ‘chatgpt’
+
+// Output: Odd
+
+// Problem 03 : তোমাকে একটা function দেওয়া হবে called “isLGSeven()”. এটা ইনপুট হিসেবে একটা number নিবে।  
+
+
+
+// এখন তোমার task: তোমাকে ইনপুট ভ্যালু এবং ৭ এর মধ্যে পার্থক্য বের করতে হবে। যদি এই পার্থক্য ৭ এর চেয়ে ছোট হয়, তখন তোমাকে return করতে হবে সেই বিয়োগফল। নাহলে তোমাকে return করতে হবে double of the input। মানে যে সংখ্যা ইনপুট হিসেবে নিয়েছো সেটির দ্বিগুণ।
+
+// Input : 6
+
+// Output: -1
+
+// Input: -15
+
+// Output: -22
+
+// Input: 15
+
+// Output: 30
+
+function isLGSeven(num){
+    let i = num - 7;
+    if(i < 7){
+        return i;
+    }
+    else{
+        return num*2;
+    }
+}
+const getValue = isLGSeven(15);
+console.log(getValue);
+
+
+
+
+
+// Problem 4: Finding Bad data
+
+
+// তোমাকে একটা function দেওয়া হবে called findingBadData()”. এটা ইনপুট হিসেবে একটা array নিবে। array তে যেকোনো সংখ্যক কতগুলো number থাকবে। number গুলো negative number (less than zero) ও হতে পারে, আবার positive number (greater than or equal to zero) ও হতে পারে। কোনও number যদি negative হয় সেটাকে আমরা বলব “Bad Data”. কোনও number যদি positive হয় সেটাকে আমরা বলব “Good Data”. এখন তোমার task: array তে কতগুলো Bad data আছে সেটা খুঁজে বের করতে হবে এবং সেই নাম্বার টা return করতে হবে।
+function findingBadData(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+ const getData = findingBadData([ -4, -9, -5, -33, -55 ]);
+ console.log(getData);
+
+// Sample Input & Output:-
+
+
+
+// Input: [ 1,2,5 ]
+
+// Output: 0
+
+
+
+// Input: [ 2, -5, -7, -13 ]
+
+// Output: 3
+
+
+
+// Input: [ -4, -9, -5, -33, -55 ]
+
+// Output: 5
+
+
+
+
+
+// Problem 5: Convert your gems into diamond
+
+
+// তোমাকে একটা function দেওয়া হবে called gemsToDiamond()”. এটা ইনপুট হিসেবে তিনটা number ( তোমাদের ৩ বন্ধুর gems এর সংখ্যা ) নিবে। 
+
+
+
+// ১ম বন্ধুর প্রতি gems এর ক্ষমতা = 21
+
+// ২য় বন্ধুর প্রতি gems এর ক্ষমতা = 32
+
+// ৩য় বন্ধুর প্রতি gems এর ক্ষমতা = 43
+
+
+
+// [ gems এর ক্ষমতা বলতে একটা gems কে কয়টা diamond এ convert করা যাবে সেটা বুঝানো হচ্ছেএখন তোমার task: সব বন্ধুর gems মিলিয়ে total কতটি diamond পাবে সেটা বের করতে হবে। total diamond এর সংখ্যা যদি 1000 এর দিগুন এর উপর হয়ে যায় সেক্ষেত্রে total diamond থেকে 2000 বাদ দিয়ে যতগুলো diamond অবশিষ্ট থাকবে, ততগুলো তোমরা পাবে।Sample Input & Output:-
+
+function gemsToDiamond(f1gem, f2gem, f3gem){
+  let  i= f1gem*21 + f2gem*32 +f3gem*43;
+    if(i<=1000*2){
+        return i;
+    }
+    else if(i >1000*2){
+        return (i-2000);
+    }
+}
+const getDiamond = gemsToDiamond(100, 5, 1);
+console.log(getDiamond);
+
+
+// Input: 1, 1, 1
+
+// Output: 96
+
+
+
+// Input: 20, 200, 50
+
+// Output: 6970
+
+
+
+// Input: 100, 5, 1
+
+// Output: 303
+
