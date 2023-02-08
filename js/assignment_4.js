713c257148ea8fd1dd78e8f32ae5dd83165b2524
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-02-08 11:08:08 +0600

    some update code

--- js/assignment_4.js
+++ js/assignment_4.js
@@ -1,16 +1,24 @@
 // Assignment 04
 
 // Problem 01
-// Solutions
+// Solutions:
 
 function mindGame(number){
     result = (number * 3 + 10)/2 - 5;
     return result;
 
 }
+function validateAndPrint(number) {
+  if (typeof number != number) {
+    console.log("Invalid input. Please enter a string.");
+  } else {
+    
+    console.log(mindGame(number));
+  }
+}
 
-const game = mindGame(50);
-console.log(game);
+  const game = mindGame("nizam");
+  console.log(game);
 
 // Descriptions: The function mindGame takes a positive number num as input and returns the result of the calculation described in this problem. The calculation is performed using a single expression and the function returns the result.
 
@@ -20,19 +28,36 @@ console.log(game);
 // Solution:
 
 
-function evenOdd(str) {
-    return str.length % 2 === 0 ? 'even' : 'odd';
-  }
-  const getEvenOrOdd = evenOdd('chatgpt');
-  console.log(getEvenOrOdd); 
 
+
+    // function evenOdd(str) {
+    //     return str.length % 2 === 0 ? 'even' : 'odd';
+          
+    //     }
+    function evenOdd(str) {
+      return str.length % 2 === 0 ? 'even' : 'odd';
+    }
+    
+    function validateAndPrint(str) {
+      if (typeof str === "string") {
+        console.log(evenOdd(str));
+      } else {
+        console.log("Invalid input. Please enter a string.");
+      }
+    }
+    
+    //  validateAndPrint(122); // outputs "odd"
+    // validateAndPrint(123); // outputs "Invalid input. Please enter a string."
+    
+    
+    
 //   Description:
 //   The function evenOdd takes a string str as input and returns 'even' if the number of characters in the string is even, and 'Odd' if it's odd. The length of the string is calculated using the .length property, and the modulo operator % is used to determine if it's even or odd. The ternary operator ? is used to return 'even' if the length is even and 'odd' otherwise.
 
 
 
 //   Problem 03
-// Solution
+// Solution:
 
 
 function isLGSeven(number){
@@ -44,8 +69,9 @@ function isLGSeven(number){
         return number*2;
     }
 }
-const getValue = isLGSeven(15);
-console.log(getValue);
+
+//  const getValue = isLGSeven(15);
+//  console.log(getValue);
 
 
 // Descriptions: The function isLGSeven takes a number as input and returns the result of the calculation described in the problem. The variable i is used to store the absolute value of the difference between number and 7. The ternary operator ? is used to return i if it is less than 7, and return number * 2 otherwise. 
@@ -64,8 +90,10 @@ function findingBadData(arr) {
     }
     return noOfBadData;
   }
- const getData = findingBadData([ -4, -9, -5, -33, -55 ]);
- console.log(getData);
+
+
+  // const getData = findingBadData([ -4, -9, -5, -33, -55 ]);
+  // console.log(getData);
 
 // Descriptions: The function findingBadData takes an array arr as input and returns the number of negative elements in the array. A variable count is used to keep track of the number of negative elements. The for loop iterates over the elements of the array, and the if statement checks if each element is negative. If an element is negative, the count variable is incremented by 1. Finally, the function returns the value of noOfBadData.
 
@@ -73,16 +101,24 @@ function findingBadData(arr) {
 // Solution:
 
 
-function gemsToDiamond(f1gem, f2gem, f3gem){
-    let  i= f1gem*21 + f2gem*32 +f3gem*43;
-      if(i<=1000*2){
-          return i;
+function gemsToDiamond(friend1gem, friend2gem, friend3gem){
+    let  total= friend1gem*21 + friend2gem*32 +friend3gem*43;
+      if(total < 1000*2){
+          return total;
       }
-      else if(i >1000*2){
-          return (i-2000);
+      else if(total >= 1000*2){
+          return (total-2000);
       }
   }
-  const getDiamond = gemsToDiamond(100, 5, 1);
-  console.log(getDiamond);
+
+
+    // const getDiamond = gemsToDiamond(100, 5, 1);
+    // console.log(getDiamond);
 
 //   Descriptions: The function gemsToDiamond takes three numbers f1gem, f2gem, and f3gem as input and returns the total number of diamonds you will get by combining the gems of all your friends. The variable total is used to store the i number of diamonds, which is calculated by multiplying the number of gems for each friend by the power of gems per friend and summing the results. The ternary operator ? is used to return i - 2000 if total is greater than 1000*2, and return total otherwise.
+
+
+/* Submitted By 
+   Md Nizam Uddin  
+   Batch 07 */
+   
--- js/milstone4.js
+++ js/milstone4.js
@@ -1,43 +1,43 @@
 // Object
-const person = {
-    firstName: 'Nizam',
-    lastName: 'Uddin',
-    age: 30,
-    email: "nizamquan@gmail.com",
-    address: {
-        city: "Chittagong",
-        zipCode: 4203},
+// const person = {
+//     firstName: 'Nizam',
+//     lastName: 'Uddin',
+//     age: 30,
+//     email: "nizamquan@gmail.com",
+//     address: {
+//         city: "Chittagong",
+//         zipCode: 4203},
     
-    getBirthYear: function(year){
-        return 2022-this.age;
-        // return 1982;
-    }
-}
+//     getBirthYear: function(year){
+//         return 2022-this.age;
+//         // return 1982;
+//     }
+// }
 
-let value;
-value = person;
-// get specific value
-value = person.firstName;
-value = person['lastName'];
-value = person.age;
-value = person.address.city;
-value = person.address.zipCode;
-value = person.getBirthYear();
+// let value;
+// value = person;
+// // get specific value
+// value = person.firstName;
+// value = person['lastName'];
+// value = person.age;
+// value = person.address.city;
+// value = person.address.zipCode;
+// value = person.getBirthYear();
 
-const people = [
-    {name: "Arefa", age: 32},
-    {name: "Nahian", age: 07},
-    {name: "Nahar", age: 67},
-];
+// const people = [
+//     {name: "Arefa", age: 32},
+//     {name: "Nahian", age: 07},
+//     {name: "Nahar", age: 67},
+// ];
 
-for(let i = 0; i<people.length; i++){
-    console.log(people[i].name)
-}
-
-
+// for(let i = 0; i<people.length; i++){
+//     console.log(people[i].name)
+// }
 
 
-console.log(value);
+
+
+// console.log(value);
 
 // Javascript math calculation
 let value2;
@@ -129,24 +129,24 @@ console.log(value3);
 // }
 // test('nizam', 'uddin');
 
-function test(a='jon', b='doe'){
-    // if(typeof a == 'undefined'){
-    //     a = 'jon';
-    // } if(typeof b == 'undefined'){
-    //     b = 'Doe'
-    // }
-    console.log('hello' + a + b);
-}
-// test('Nizam', 'uddon');
-test();
+// function test(a='jon', b='doe'){
+//     // if(typeof a == 'undefined'){
+//     //     a = 'jon';
+//     // } if(typeof b == 'undefined'){
+//     //     b = 'Doe'
+//     // }
+//     console.log('hello' + a + b);
+// }
+// // test('Nizam', 'uddon');
+// test();
 
 // Returning a value
 
-function sqare(x){
-    return (x*x);
-}
-let sqareFour = sqare(4);
-console.log(sqareFour);
+// function sqare(x){
+//     return (x*x);
+// }
+// let sqareFour = sqare(4);
+// console.log(sqareFour);
 
 // Naming function
 // getValue 
@@ -154,21 +154,21 @@ console.log(sqareFour);
 // calSquare
 // immidiately invokable function expression
 
-(function(name){
-    console.log(name);
-})('Nizam');
+// (function(name){
+//     console.log(name);
+// })('Nizam');
 
-// for loop
+// // for loop
 
 // for(let i = 0; i <=10; i++){
 // for(let i = 0; i <10; i=i+1){
 // for(let i = 0; i <10; i+=2){
-for(let i = 9; i >0; i--){
-    if(i===5) break;//continue;
-    console.log(i);
-}
+// for(let i = 9; i >0; i--){
+//     if(i===5) break;//continue;
+//     console.log(i);
+// }
 
-let myArray = ['one', 'two', 'three', 'four', 'five'];
+// let myArray = ['one', 'two', 'three', 'four', 'five'];
 // for(let i = 0; i< myArray.length; i++){
 //  for(let i= myArray.length-1; i >=0; i--){
 //     console.log(myArray[i]);
@@ -182,11 +182,11 @@ let myArray = ['one', 'two', 'three', 'four', 'five'];
 // }
 
 
-let i=0;
-while(i< myArray.length){
-    console.log(myArray[i]);
-    i++;
-}
+// let i=0;
+// while(i< myArray.length){
+//     console.log(myArray[i]);
+//     i++;
+// }
 
 // Assignment
 // Problem 01: তোমাকে একটা function দেওয়া হবে called mindGame(” যা ইনপুট হিসেবে একটা positive number নিবে। 
@@ -207,13 +207,13 @@ while(i< myArray.length){
 
 // Output: -25.5
 
-function mindGame(number){
-    result = (number * 3 + 10)/2-5;
-    return result;
+// function mindGame(number){
+//     result = (number * 3 + 10)/2-5;
+//     return result;
 
-}
-const game = mindGame(50);
-console.log(game);
+// }
+// const game = mindGame(50);
+// console.log(game);
 
 
 // Problem 02: তোমাকে একটা function দেওয়া হবে called evenOdd()”. এটা ইনপুট হিসেবে একটা string নিবে। 
@@ -276,7 +276,7 @@ function isLGSeven(num){
 const getValue = isLGSeven(15);
 console.log(getValue);
 
-
+// Descriptions: The function isLGSeven takes a number num as input and returns the result of the calculation described in the problem. The variable i is used to store the absolute value of the difference between num and 7. The ternary operator ? is used to return i if it is less than 7, and return num * 2 otherwise. 
 
 
 
@@ -284,17 +284,17 @@ console.log(getValue);
 
 
 // তোমাকে একটা function দেওয়া হবে called findingBadData()”. এটা ইনপুট হিসেবে একটা array নিবে। array তে যেকোনো সংখ্যক কতগুলো number থাকবে। number গুলো negative number (less than zero) ও হতে পারে, আবার positive number (greater than or equal to zero) ও হতে পারে। কোনও number যদি negative হয় সেটাকে আমরা বলব “Bad Data”. কোনও number যদি positive হয় সেটাকে আমরা বলব “Good Data”. এখন তোমার task: array তে কতগুলো Bad data আছে সেটা খুঁজে বের করতে হবে এবং সেই নাম্বার টা return করতে হবে।
-function findingBadData(arr) {
-    let count = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < 0) {
-        count++;
-      }
-    }
-    return count;
-  }
- const getData = findingBadData([ -4, -9, -5, -33, -55 ]);
- console.log(getData);
+// function findingBadData(arr) {
+//     let count = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] < 0) {
+//         count++;
+//       }
+//     }
+//     return count;
+//   }
+//  const getData = findingBadData([ -4, -9, -5, -33, -55 ]);
+//  console.log(getData);
 
 // Sample Input & Output:-
 
@@ -337,18 +337,29 @@ function findingBadData(arr) {
 
 // [ gems এর ক্ষমতা বলতে একটা gems কে কয়টা diamond এ convert করা যাবে সেটা বুঝানো হচ্ছেএখন তোমার task: সব বন্ধুর gems মিলিয়ে total কতটি diamond পাবে সেটা বের করতে হবে। total diamond এর সংখ্যা যদি 1000 এর দিগুন এর উপর হয়ে যায় সেক্ষেত্রে total diamond থেকে 2000 বাদ দিয়ে যতগুলো diamond অবশিষ্ট থাকবে, ততগুলো তোমরা পাবে।Sample Input & Output:-
 
-function gemsToDiamond(f1gem, f2gem, f3gem){
-  let  i= f1gem*21 + f2gem*32 +f3gem*43;
-    if(i<=1000*2){
-        return i;
-    }
-    else if(i >1000*2){
-        return (i-2000);
-    }
-}
-const getDiamond = gemsToDiamond(100, 5, 1);
-console.log(getDiamond);
-
+// function gemsToDiamond(f1gem, f2gem, f3gem){
+//   let  i= f1gem*21 + f2gem*32 +f3gem*43;
+//     if(i<=1000*2){
+//         return i;
+//     }
+//     else if(i >1000*2){
+//         return (i-2000);
+//     }
+// }
+// const getDiamond = gemsToDiamond(100, 5, 1);
+// console.log(getDiamond);
+// function gemsToDiamond(friend1, friend2, friend3) {
+//     const totalDiamonds = (friend1 * 21) + (friend2 * 32) + (friend3 * 43);
+  
+//     if (totalDiamonds > 1000) {
+//       console.log("Total diamonds: " + (totalDiamonds - 2000));
+//     } else {
+//       console.log("Total diamonds: " + totalDiamonds);
+//     }
+//   }
+  
+//   gemsToDiamond(5, 10, 15);
+  
 
 // Input: 1, 1, 1
 
@@ -366,3 +377,168 @@ console.log(getDiamond);
 
 // Output: 303
 
+function compare(a, b) {
+
+    if (a == b) {
+   
+    return true;
+   
+    } else {
+   
+    return false;
+   
+    }
+   
+   }
+   
+   const result = compare(15, "15");
+   
+   console.log(result);
+
+   
+//    Bonus Video
+
+// Video 24_5-1 Understand recursion using sum of numbers
+// let sum = 0;
+// for(let i = 1; i <= 5; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// let sum = 0;
+// for(let i = 5; i >= 1; i--){
+//     sum = sum +i;
+// }
+// console.log(sum);
+ function sum(i){
+    if(i == 1){
+        return 1;
+    }
+    console.log(i);
+    return i + sum(i-1);
+ }
+ const result2 = sum(5);
+ console.log(result2);
+
+//  Video 24_5-2 Explore Factorial Recursion using a for loop concept
+/* 
+5 + sum(i-1)
+5 + sum(5-1)
+5 + 4 + i + sum ( i - 1)
+5+4+3+ sum(i-1)
+5+3+2+ sum(i-1)
+5+4+3+2+1
+*/
+// Factorial
+// let factorial = 1;
+// for(let i = 5; i>1; i--){
+//     factorial = factorial * i;
+// }
+// console.log(factorial);
+
+function factorial(i){
+    if(i == 1){
+        return 1;
+    }
+    return i * factorial(i-1);
+}
+const result1 = factorial(5);
+console.log(result1);
+/*
+5 * factorial(4)
+5* 4 * factorial(3)
+5 * 4 * 3 * factorial (2)
+5 * 4 * 3 * 2  * factorial(1)
+5*4*3*2*1
+*/
+
+// Video 24_5-3 (optional) Explore what you can do with JavaScript Object
+
+const nayok = {
+    name : 'Sakib khan',
+    id: 121,
+    address: 'movie cinema',
+    isSingle: true,
+    friend: ['Apu', 'Razz', 'Salman', 'Amir'],
+    moviea: [{name: 'no1', year: 2015}, {name: 'King khan', year: 1018}],
+    act: function(){
+        console.log('acting like sakib khan')
+    },
+    car: {
+        brand: 'tesla',
+        price: 50000000,
+        made: 2025,
+        manufacture:{
+            name: 'tesla',
+            ceo: 'Elon mask',
+            country: 'USA'
+        }
+    }
+}
+// console.log(nayok.car);
+console.log(nayok.act);
+nayok.act();
+
+const products = [
+    {name: 'phone', price: '12000'},
+    {name: 'laptop', price: '32000'},
+]
+// products[0]
+// products[1]
+// const products = [15, 56,87]
+// array like objects
+const products1 = {
+    '0': 15,
+    '1': 56,
+    '2': 87,
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+    console.log(arguments[4]);
+}
+add(12, 13, 45, 89, 78);
+
+// Video 24_5-4: (advanced) Find the matching product by searching products
+
+const numbers1 = [ 45, 65, 23, 98, 19];
+// for(let i = 0; i < numbers1.length; i++){
+//     const number = numbers1[i];
+//     console.log(numbers1);
+// }
+
+// for of loop
+for(const number of numbers1){
+    // console.log(number);
+}
+
+const products3 = [
+    {id: 1, name: 'xiami phone onenight', price: 19000},
+    {id: 2, name: 'iphone', price: 59000},
+    {id: 3, name: 'mac book air', price: 119000},
+    {id: 4, name: 'lenovo yoga laptop 2025', price: 19000},
+    {id: 5, name: 'Dell inspirotion laptop', price: 19000},
+    {id: 6, name: 'Samsung phone note 7', price: 19000},
+    {id: 7, name: 'Nokia old age phone gone', price: 19000},
+    {id: 8, name: 'Phone one', price: 19000},
+];
+
+// for(const product of products3){
+//     console.log(product);
+// }
+
+function matchedProducts (products, search){
+    const matched = [];
+    for(const product of products3){
+        if(product.name.toLowerCase().includes(search.toLowerCase())){
+            matched.push(product);
+        }
+        // console.log(product.name.includes(search));
+    }
+    return matched;
+}
+
+const result5 = matchedProducts(products3, 'laptop');
+console.log(result5);
+
+// Video 24_5-5 : Problem solving priority and get best out of this course
